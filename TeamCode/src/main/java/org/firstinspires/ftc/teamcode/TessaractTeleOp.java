7b7ec3a0023efe2055a7a5c03695c00f1bf3a3fe
RPHS-Robotics <rockypointhsrobotics@gmail.com>
2024-11-07 15:03:40 -0500

    Update TessaractTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TessaractTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TessaractTeleOp.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.google.blocks.ftcrobotcontroller.runtime.ImuAccess;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
+import java.security.KeyPairGenerator;
 
 @TeleOp(name="Tessaract")
 public class TessaractTeleOp extends OpMode {
@@ -18,6 +21,7 @@ public class TessaractTeleOp extends OpMode {
     public Servo claw;
     public Vector2D lJoyPos = Vector2D.ZERO;
     double armSpeed = 2;
+    private KeyPairGenerator imu;
 
     @Override
     public void init() {
@@ -27,21 +31,44 @@ public class TessaractTeleOp extends OpMode {
         bRMotor = hardwareMap.get(DcMotor.class, "BR");
         armMotor = hardwareMap.get(DcMotor.class, "ARM");
         claw = hardwareMap.get(Servo.class, "CLAW");
+        imu = hardwareMap.get(IMU.class, "imu");
+        imuParams = new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                        RevHubOrientationOnRobot.UsbFacingDirection.UP
+                )
+        );
+        imu.initialize(imuParams);
+        imu.resetYaw();
     }
 
     @Override
     public void loop() {
         Vector2D lJoyPos = new Vector2D(gamepad1.left_stick_x, gamepad1.left_stick_y);
-        double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
+        double y = gamepad1.left_stick_y; // Remember, Y stick is reversed!
         double x = gamepad1.left_stick_x;
+        double lJoyAngle = Math.atan2(y, x);
+        double lJoyDistance = Math.sqrt(x * x + y * y);
         double rx = gamepad1.right_stick_x;
+
+
+
+
+
+
+        //double Yaw   = robotOrientation.getYaw(AngleUnit.DEGREES);
+
+
+
+
+
 
         // Calculates movement power
 
-        fLMotor.setPower(y + x + rx);
-        bLMotor.setPower(y - x + rx);
-        fRMotor.setPower(y - x - rx);
-        bRMotor.setPower(y + x - rx);
+        fLMotor.setPower(-y + x + rx);
+        bLMotor.setPower(-y - x + rx);
+        fRMotor.setPower(-y - x - rx);
+        bRMotor.setPower(-y + x - rx);
 
         // Arm speed on dPad press
 
